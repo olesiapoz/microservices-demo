8ac90b1b5cbe9c4f3f886b2fbe16dd8cfd374e41
olesiapoz <test@gitmail>
2018-09-26 22:45:25 +0300

    Payment function functional

--- cmd/paymentserverless/main.go
+++ cmd/paymentserverless/main.go
@@ -1,111 +1,100 @@
 package main
 
-import "encoding/json"
-import "fmt"
-import "os"
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	
-	"github.com/go-kit/kit/log"
-	"github.com/olesiapoz/microservices-demo/payment"
-	stdopentracing "github.com/opentracing/opentracing-go"
-	zipkin "github.com/openzipkin/zipkin-go-opentracing"
-	"golang.org/x/net/context"
+	//      "os"
+	//      "strconv"
+	//      "github.com/olesiapoz/microservices-demo"
 )
-func main() {
-    //program receives one argument: the JSON object as a string
-    arg := os.Args[1]
 
-    // unmarshal the string to a JSON object
-    var req map[string]interface{}
+func decodeAuthoriseRequest(arg string) float32 {
+	var req map[string]interface{}
 	json.Unmarshal([]byte(arg), &req)
 
-	ammount, ok := req["ammont"].(string)
-	
-	// last line of stdout is the result JSON object as a string
-	msg := map[string]string{"ammount": (ammount)}
-	res, _ := json.Marshal(msg)
-	fmt.Println(string(res))
-	
-	/*var (
-		declineAmount = flag.Float64("decline", 105, "Decline payments over certain amount")
-	)
+	// Decode auth request
 
-	func decodeAuthoriseRequest(r *arg) (interface{}, error) {
-		// Read the content
-		var bodyBytes []byte
-		if r != nil {
-			var err error
-			bodyBytes, err = ioutil.ReadAll(r)
-			if err != nil {
-				return nil, err
-			}
-		}
-		// Save the content
-		bodyString := string(bodyBytes)
-	
-		// Decode auth request
-		var request AuthoriseRequest
-		if err := json.Unmarshal(bodyBytes, &arg); err != nil {
-			return nil, err
-		}
-	
-		// If amount isn't present, error
-		if arg.Amount == 0.0 {
-			return nil, &UnmarshalKeyError{
-				Key:  "amount",
-				JSON: bodyString,
-			}
-		}
-		return request, nil
+	//json.Unmarshal(arg, &req)
+	am, ok := req["amount"].(float64)
+	if !ok {
+		return 0
 	}
-	
-	type UnmarshalKeyError struct {
-		Key  string
-		JSON string
-	}
-	
-	func (e *UnmarshalKeyError) Error() string {
-		return fmt.Sprintf("Cannot unmarshal object key %q from JSON: %s", e.Key, e.JSON)
-	}
-	
-	var ErrInvalidJson = errors.New("Invalid json")
-	
-	func encodeAuthoriseResponse(response interface{}) error {
-		resp := response.(AuthoriseResponse)
-		if resp.Err != nil {
-			encodeError(resp.Err)
-			return nil
-		}
-		return encodeResponse(resp.Authorisation)
-	}
-	
-	func encodeResponse(response interface{}) error {
-		// All of our response objects are JSON serializable, so we just do that.
-		return json.Encode(response)
+	return float32(am)
+}
 
-	}
+type UnmarshalKeyError struct {
+	Key  string
+	JSON string
+}
+
+func (e *UnmarshalKeyError) Error() string {
+	return fmt.Sprintf("Cannot unmarshal object key %q from JSON: %s", e.Key, e.JSON)
+}
+
+var ErrInvalidJson = errors.New("Invalid json")
 
 // AuthoriseRequest represents a request for payment authorisation.
 // The Amount is the total amount of the transaction
-type AuthoriseRequest struct {
-	Amount float32 `json:"amount"`
+
+type Service interface {
+	Authorise(total float32) (Authorisation, error) // GET /paymentAuth
+	//      Health() []Health                               // GET /health
 }
 
-// AuthoriseResponse returns a response of type Authorisation and an error, Err.
-type AuthoriseResponse struct {
-	Authorisation Authorisation
-	Err           error
+type Authorisation struct {
+	Authorised bool   `json:"authorised"`
+	Message    string `json:"message"`
 }
 
-    // last line of stdout is the result JSON object as a string
-
+func NewAuthorisationService(declineOverAmount float32) Service {
+	return &service{
+		declineOverAmount: declineOverAmount,
+	}
 }
-*/
+
+//ammount of money that shoudl be declined
+type service struct {
+	declineOverAmount float32
+}
+
+func (s *service) Authorise(amount float32) (Authorisation, error) {
+	if amount == 0 {
+		return Authorisation{}, ErrInvalidPaymentAmount
+	}
+	if amount < 0 {
+		return Authorisation{}, ErrInvalidPaymentAmount
+	}
+	authorised := false
+	message := "Payment declined"
+	if amount <= s.declineOverAmount {
+		authorised = true
+		message = "Payment authorised"
+	} else {
+		message = fmt.Sprintf("Payment declined: amount exceeds %.2f", s.declineOverAmount)
+	}
+	return Authorisation{
+		Authorised: authorised,
+		Message:    message,
+	}, nil
+}
+
+//error
+var ErrInvalidPaymentAmount = errors.New("Invalid payment amount")
+
+func main() {
+	//program receives one argument: the JSON object as a string
+	var declineAmount = flag.Float64("decline", 105, "Decline payments over certain amount")
+	arg := os.Args[1]
+
+	service := NewAuthorisationService(float32(*declineAmount))
+	// unmarshal the string to a JSON object
+
+	amount := decodeAuthoriseRequest(arg)
+	auth, _ := service.Authorise(amount)
+
+	json, _ := json.Marshal(auth)
+	fmt.Println(string(json))
+}
